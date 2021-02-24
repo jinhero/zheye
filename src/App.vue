4e7d7a395873e069222195b4381f53cd91635f88
肖金浩 <1501942779@qq.com>
2021-02-24 10:28:38 +0800

    抽象验证规则

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,7 @@
     <form action>
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
-        <input type="text" class="form-control" id="exampleInputEmail1" v-model="emailRef.val" @blur="validateEmail" />
-        <div class="form-text" v-if="emailRef.error">{{emailRef.message}}</div>
+        <validate-input :rules="emailRules"></validate-input>
       </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">密码</label>
@@ -18,9 +17,10 @@
 
 <script lang="ts">
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { defineComponent, reactive } from 'vue'
+import { defineComponent } from 'vue'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
 const currentUser: UserProps = {
   isLogin: true,
   name: 'viking'
@@ -53,33 +53,22 @@ const testData: ColumnProps[] = [
     avatar: ''
   }
 ]
-const emailReg = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/
 export default defineComponent({
   name: 'App',
   components: {
     ColumnList,
-    GlobalHeader
+    GlobalHeader,
+    ValidateInput
   },
   setup () {
-    const emailRef = reactive({
-      val: '',
-      error: false,
-      message: ''
-    })
-    const validateEmail = () => {
-      if (emailRef.val.trim() === '') {
-        emailRef.error = true
-        emailRef.message = 'can not be empty'
-      } else if (!emailReg.test(emailRef.val)) {
-        emailRef.error = true
-        emailRef.message = 'should be valid email'
-      }
-    }
+    const emailRules: RulesProp = [
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱格式' }
+    ]
     return {
       list: testData,
       currentUser,
-      emailRef,
-      validateEmail
+      emailRules
     }
   }
 })
